2db21d0dbb7565d73e6b5ae3b949dea26397959a
Dario Safa <dario.akhavansafa@uzh.ch>
2023-01-26 00:38:49 +0100

    init

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -11,13 +12,21 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <div>
+          <Image
+            src="/images/9egg.png"
+            height={200}
+            width={500}
+            fill
+            sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+          />
+        </div>
+        <Header title="Welcome to 9egg!" />
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
